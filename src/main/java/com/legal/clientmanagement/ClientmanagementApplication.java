e2fe3123ac71eabd11b5c642cd17b1bb697f2236
anjaneyadatla <anjaneyalndatla@gmail.com>
2018-06-11 18:45:21 -0400

    first commit

---
+++ src/main/java/com/legal/clientmanagement/ClientmanagementApplication.java
@@ -0,0 +1,12 @@
+package com.legal.clientmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientmanagementApplication.class, args);
+	}
+}
---
+++ src/test/java/com/legal/clientmanagement/ClientmanagementApplicationTests.java
@@ -0,0 +1,16 @@
+package com.legal.clientmanagement;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ClientmanagementApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
